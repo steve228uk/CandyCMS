675d2db833abd3dba3859852f6318861cba60d29
Stephen Radford <steve2288uk@gmail.com>
2012-04-25 19:45:10 +0100

    Added regex to Twitter widget
    Added preg_replace to wrap @replies, #tags and links in <a> tags.

--- plugins/Twitter/widget.php
+++ plugins/Twitter/widget.php
@@ -11,7 +11,13 @@ $account = Twitter::twitterAccount();
 $tweets = simplexml_load_file("https://api.twitter.com/1/statuses/user_timeline.xml?include_entities=true&include_rts=true&screen_name=$account&count=1");
 
 foreach ($tweets as $tweet) {
-	echo '<p>', $tweet->text, '</p>';
+
+	$tweettext = $tweet->text;
+	$tweettext = preg_replace('/(http:\/\/[a-z0-9\.\/]+)/i', '<a href="$1" target="_blank">$1</a>', $tweettext);
+	$tweettext = preg_replace('/( @|^@)(\w+)/', '$1<a rel="nofollow" href="http://www.twitter.com/$2" target="_blank" title="Follow $2 on Twitter">$2</a>', $tweettext); 
+	$tweettext = preg_replace('/( #|^#)(\w+)/', '$1<a rel="nofollow" href="https://twitter.com/#!/search?q=%23$2" target="_blank" title="$2">$2</a>', $tweettext); 
+	
+	echo '<p>', $tweettext, '</p>';
 } ?>
 <div>
 	<a href="https://twitter.com/<?php echo $account ?>" class="twitter-follow-button" data-show-count="false">Follow @<?php echo $account ?></a>
