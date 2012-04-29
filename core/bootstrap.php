1fe4992c48c609ded2effde3f1b79b6ba63c3d46
Stephen Radford <steve228uk@gmail.com>
2012-04-29 17:01:06 +0200

    Update core/bootstrap.php

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,13 +2,13 @@
 
 /**
 * @package CandyCMS
-* @version 0.2.1
+* @version 0.2.2
 * @copyright Copyright 2012 (C) Cocoon Design Ltd. - All Rights Reserved
 * 
 * This file is the main bootstrap for CandyCMS
 */
 
-define('CANDYVERSION', '0.2.1');
+define('CANDYVERSION', '0.2.2');
 
 /**
  * Set the development enviornment
