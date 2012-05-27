da64f252d207fee05f99d01402ea14d954518de8
Stephen Radford <steve2288uk@gmail.com>
2012-05-27 13:36:08 +0100

    Updated version number
    Updated CANDYVERSION to 0.5 in both bootstraps

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,13 +2,14 @@
 
 /**
 * @package CandyCMS
-* @version 0.3
+* @version 0.5
+* @since 0.1
 * @copyright Copyright 2012 (C) Cocoon Design Ltd. - All Rights Reserved
 * 
 * This file is the main bootstrap for CandyCMS
 */
 
-define('CANDYVERSION', '0.4');
+define('CANDYVERSION', '0.5');
 
 /**
  * Set the development enviornment
