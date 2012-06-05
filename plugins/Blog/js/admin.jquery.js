de136e2043b815e1ae3ee1929c55dbbf13c45446
Stephen Radford <steve2288uk@gmail.com>
2012-06-05 14:14:20 +0100

    Disabled certain categories from being added
    Blank categories or categories that are a number can no longer be added
    as they conflict with the pagination

--- plugins/Blog/js/admin.jquery.js
+++ plugins/Blog/js/admin.jquery.js
@@ -3,18 +3,28 @@ $(function(){
 	$('#addcat').click(function() {
 		
 			var cat = jQuery('#newcat').val();
-		
-			jQuery.post("../core/ajax.php", { action: "Blog", catname: cat},
+			
+			if (jQuery('#newcat').val() == '') {
+				jQuery('#newcat').addClass('error');
+			} else if(isNaN(jQuery('#newcat').val())) {
+			
+				jQuery('#newcat').removeClass('error');
+			
+				jQuery.post("../core/ajax.php", { action: "Blog", catname: cat},
+					
+					function(data) {
+						
+						jQuery('<li>' + cat + '<input type="checkbox" name="categories[]" value="' + data + '" checked="checked" />').appendTo('#blog-cats ul');
+						
+						jQuery('#newcat').val('');
+						
+					}
+					
+				);
 				
-				function(data) {
-					
-					jQuery('<li>' + cat + '<input type="checkbox" name="categories[]" value="' + data + '" checked="checked" />').appendTo('#blog-cats ul');
-					
-					jQuery('#newcat').val('');
-					
-				}
-				
-			);
+			} else {
+				jQuery('#newcat').addClass('error');
+			}
 		
 		
 		return false;
