2f741b3eb6276f5dad95f44e804d59a2c94e7a44
Stephen Radford <steve2288uk@gmail.com>
2012-06-09 22:33:22 +0100

    0.6-dev1

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,14 +2,14 @@
 
 /**
 * @package CandyCMS
-* @version 0.5.3
+* @version 0.6
 * @since 0.1
 * @copyright Copyright 2012 (C) Cocoon Design Ltd. - All Rights Reserved
 * 
 * This file is the main bootstrap for CandyCMS
 */
 
-define('CANDYVERSION', '0.5.3');
+define('CANDYVERSION', '0.6-dev1');
 
 /**
  * Set the development enviornment
