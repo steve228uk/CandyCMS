7eea853ff877134d21c7b111ea630998244e7e23
Stephen Radford <steve2288uk@gmail.com>
2012-06-10 17:49:49 +0100

    Updated Twitter widget regex
    Included https

--- plugins/Twitter/widget.php
+++ plugins/Twitter/widget.php
@@ -15,6 +15,7 @@ foreach ($tweets as $tweet) {
 
 	$tweettext = $tweet->text;
 	$tweettext = preg_replace('/(http:\/\/[a-z0-9\.\/]+)/i', '<a href="$1" target="_blank">$1</a>', $tweettext);
+	$tweettext = preg_replace('/(https:\/\/[a-z0-9\.\/]+)/i', '<a href="$1" target="_blank">$1</a>', $tweettext);
 	$tweettext = preg_replace('/( @|^@)(\w+)/', '$1<a rel="nofollow" href="http://www.twitter.com/$2" target="_blank" title="Follow $2 on Twitter">$2</a>', $tweettext); 
 	$tweettext = preg_replace('/( #|^#)(\w+)/', '$1<a rel="nofollow" href="https://twitter.com/#!/search?q=%23$2" target="_blank" title="$2">$2</a>', $tweettext); 
 	
