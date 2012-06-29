ae93df2746daeb76f75198d1a47494b92616b372
Stephen Radford <steve2288uk@gmail.com>
2012-06-29 22:27:30 +0100

    Updated nestable.js

--- cms-admin/js/jquery.nestable.js
+++ cms-admin/js/jquery.nestable.js
@@ -4,8 +4,27 @@
  */
 ;(function($, window, document, undefined)
 {
-
     var hasTouch = 'ontouchstart' in window;
+
+    /**
+     * Detect CSS pointer-events property
+     * events are normally disabled on the dragging element to avoid conflicts
+     * https://github.com/ausi/Feature-detection-technique-for-pointer-events/blob/master/modernizr-pointerevents.js
+     */
+    var hasPointerEvents = (function()
+    {
+        var el    = document.createElement('div'),
+            docEl = document.documentElement;
+        if (!('pointerEvents' in el.style)) {
+            return false;
+        }
+        el.style.pointerEvents = 'auto';
+        el.style.pointerEvents = 'x';
+        docEl.appendChild(el);
+        var supports = window.getComputedStyle && window.getComputedStyle(el, '').pointerEvents === 'auto';
+        docEl.removeChild(el);
+        return !!supports;
+    })();
 
     var eStart  = hasTouch ? 'touchstart'  : 'mousedown',
         eMove   = hasTouch ? 'touchmove'   : 'mousemove',
@@ -13,13 +32,20 @@
         eCancel = hasTouch ? 'touchcancel' : 'mouseup';
 
     var defaults = {
-            listNodeName    : 'ul',
+            listNodeName    : 'ol',
             itemNodeName    : 'li',
+            rootClass       : 'dd',
+            listClass       : 'dd-list',
+            itemClass       : 'dd-item',
             dragClass       : 'dd-dragel',
             handleClass     : 'dd-handle',
             collapsedClass  : 'dd-collapsed',
             placeClass      : 'dd-placeholder',
-            maxdepth        : 3,
+            emptyClass      : 'dd-empty',
+            expandBtnHTML   : '<button data-action="expand">+</button>',
+            collapseBtnHTML : '<button data-action="collapse">-</button>',
+            group           : 0,
+            maxDepth        : 2,
             threshold       : 20
         };
 
@@ -28,9 +54,6 @@
         this.w  = $(window);
         this.el = $(element);
         this.options = $.extend({}, defaults, options);
-        this.dragEl = null;
-        this.pointEl = null;
-        this.placeEl = null;
         this.init();
     }
 
@@ -42,17 +65,19 @@
 
             list.reset();
 
+            list.el.data('nestable-group', this.options.group);
+
             list.placeEl = $('<div class="' + list.options.placeClass + '"/>');
 
             $.each(this.el.find(list.options.itemNodeName), function(k, el) {
                 list.setParent($(el));
             });
 
-            list.el.on('click', function(e) {
+            list.el.on('click', 'button', function(e) {
                 if (list.dragEl || (!hasTouch && e.button !== 0)) {
                     return;
                 }
-                var target = $(e.target),
+                var target = $(e.currentTarget),
                     action = target.data('action'),
                     item   = target.parent(list.options.itemNodeName);
                 if (action === 'collapse') {
@@ -155,7 +180,12 @@
                 distAxX   : 0,
                 distAxY   : 0
             };
-            this.moving = false;
+            this.moving     = false;
+            this.dragEl     = null;
+            this.dragRootEl = null;
+            this.dragDepth  = 0;
+            this.hasNewRoot = false;
+            this.pointEl    = null;
         },
 
         expandItem: function(li)
@@ -196,8 +226,8 @@
         setParent: function(li)
         {
             if (li.children(this.options.listNodeName).length) {
-                li.prepend('<button data-action="expand">+</button>');
-                li.prepend('<button data-action="collapse">-</button>');
+                li.prepend($(this.options.expandBtnHTML));
+                li.prepend($(this.options.collapseBtnHTML));
             }
             li.children('[data-action="expand"]').hide();
         },
@@ -211,41 +241,56 @@
 
         dragStart: function(e)
         {
-            var mouse  = this.mouse,
-                target = $(e.target);
-            this.dragEl = target.parents(this.options.itemNodeName + ':first');
+            var mouse    = this.mouse,
+                target   = $(e.target),
+                dragItem = target.parents(this.options.itemNodeName + ':first');
+
             this.placeEl.css({
-                'width'         : this.dragEl.width(),
-                'height'        : this.dragEl.height(),
-                'margin-bottom' : this.dragEl.css('margin-bottom')
+            //  'width'         : dragItem.width(),
+                'height'        : dragItem.height()
             });
+
             mouse.offsetX = e.offsetX !== undefined ? e.offsetX : e.pageX - target.offset().left;
             mouse.offsetY = e.offsetY !== undefined ? e.offsetY : e.pageY - target.offset().top;
-
             mouse.startX = mouse.lastX = e.pageX;
             mouse.startY = mouse.lastY = e.pageY;
 
-            this.dragEl.css('width', this.dragEl.width()).replaceWith(this.placeEl);
-            $(document.body).append(this.dragEl.addClass(this.options.dragClass));
+            this.dragRootEl = this.el;
+
+            this.dragEl = $(document.createElement(this.options.listNodeName)).addClass(this.options.listClass + ' ' + this.options.dragClass);
+            this.dragEl.css('width', dragItem.width());
+            dragItem.replaceWith(this.placeEl).appendTo(this.dragEl);
+            $(document.body).append(this.dragEl);
             this.dragEl.css({
                 'left' : e.pageX - mouse.offsetX,
                 'top'  : e.pageY - mouse.offsetY
             });
+            // total depth of dragging item
+            var i, depth,
+                items = this.dragEl.find(this.options.itemNodeName);
+            for (i = 0; i < items.length; i++) {
+                depth = $(items[i]).parents(this.options.listNodeName).length;
+                if (depth > this.dragDepth) {
+                    this.dragDepth = depth;
+                }
+            }
         },
 
         dragStop: function(e)
         {
-            this.reset();
-            this.placeEl.replaceWith(this.dragEl.removeClass(this.options.dragClass));
-            this.dragEl[0].style.cssText = '';
-            this.dragEl = null;
-            this.pointEl = null;
+            this.placeEl.replaceWith(this.dragEl.html());
+            this.dragEl.remove();
             this.el.trigger('change');
+            if (this.hasNewRoot) {
+                this.dragRootEl.trigger('change');
+            }
+            this.reset();
         },
 
         dragMove: function(e)
         {
-            var list, parent, prev, next,
+            var list, parent, prev, next, depth,
+                opt   = this.options,
                 mouse = this.mouse;
 
             this.dragEl.css({
@@ -277,7 +322,8 @@
                 mouse.moving = true;
                 return;
             }
-            // calc distance moved on this axis AND direction
+
+            // calc distance moved on this axis (and direction)
             if (mouse.dirAx !== newAx) {
                 mouse.distAxX = 0;
                 mouse.distAxY = 0;
@@ -296,71 +342,105 @@
             /**
              * move horizontal
              */
-            if (mouse.dirAx) {
-                if (mouse.distAxX < this.options.threshold) {
-                    return;
-                }
+            if (mouse.dirAx && mouse.distAxX >= opt.threshold) {
+                // reset move distance on x-axis for new phase
                 mouse.distAxX = 0;
-                // increase level
-                if (mouse.distX > 0) {
-                    prev = this.placeEl.prev(this.options.itemNodeName);
-                    if (!prev.length) {
-                        return;
-                    }
-                    list = prev.find(this.options.listNodeName + ':last');
-                    // item is at same level as item above
-                    if (prev.hasClass(this.options.collapsedClass)) {
-                        return;
-                    }
-                    /*
-                    var depth = placeholder.parents('li').length;
-                    if (depth >= this.options.maxdepth) {
-                        return;
-                    }
-                    */
-                    if (!list.length) {
-                        list = $('<' + this.options.listNodeName + '/>');
-                        list.append(this.placeEl);
-                        prev.append(list);
-                        this.setParent(prev);
-                    } else {
-                        // else append to next level up
-                        list = prev.children(this.options.listNodeName + ':last');
-                        list.append(this.placeEl);
-                    }
-                // decrease level
-                } else {
-                    next = this.placeEl.next(this.options.itemNodeName);
-                    if (next.length) {
-                        return;
-                    }
-                    parent = this.placeEl.parent();
-                    this.placeEl.parents(this.options.itemNodeName + ':first').after(this.placeEl);
-                    if (!parent.children().length) {
-                        this.unsetParent(parent.parent());
+                prev = this.placeEl.prev(opt.itemNodeName);
+                // increase horizontal level if previous sibling exists and is not collapsed
+                if (mouse.distX > 0 && prev.length && !prev.hasClass(opt.collapsedClass)) {
+                    // cannot increase level when item above is collapsed
+                    list = prev.find(opt.listNodeName + ':last');
+                    // check if depth limit has reached
+                    depth = this.placeEl.parents(opt.listNodeName).length;
+                    if (depth + this.dragDepth <= this.options.maxDepth) {
+                        // create new sub-level if one doesn't exist
+                        if (!list.length) {
+                            list = $('<' + opt.listNodeName + '/>').addClass(opt.listClass);
+                            list.append(this.placeEl);
+                            prev.append(list);
+                            this.setParent(prev);
+                        } else {
+                            // else append to next level up
+                            list = prev.children(opt.listNodeName + ':last');
+                            list.append(this.placeEl);
+                        }
                     }
                 }
+                // decrease horizontal level
+                if (mouse.distX < 0) {
+                    // we can't decrease a level if an item preceeds the current one
+                    next = this.placeEl.next(opt.itemNodeName);
+                    if (!next.length) {
+                        parent = this.placeEl.parent();
+                        this.placeEl.parents(opt.itemNodeName + ':first').after(this.placeEl);
+                        if (!parent.children().length) {
+                            this.unsetParent(parent.parent());
+                        }
+                    }
+                }
+            }
+
+            var isEmpty = false;
+
+            // find list item under cursor
+            if (!hasPointerEvents) {
+                this.dragEl[0].style.visibility = 'hidden';
+            }
+            this.pointEl = $(document.elementFromPoint(e.pageX - this.w.scrollLeft(), e.pageY - this.w.scrollTop()));
+            if (!hasPointerEvents) {
+                this.dragEl[0].style.visibility = 'visible';
+            }
+            if (this.pointEl.hasClass(opt.handleClass)) {
+                this.pointEl = this.pointEl.parent('.' + opt.itemClass);
+            }
+            if (this.pointEl.hasClass(opt.emptyClass)) {
+                isEmpty = true;
+            }
+            else if (!this.pointEl.length || !this.pointEl.hasClass(opt.itemClass)) {
+                return;
+            }
+
+            // find parent list of item under cursor
+            var pointElRoot = this.pointEl.parents('.' + this.options.rootClass + ':first'),
+                isNewRoot   = this.dragRootEl.data('nestable-id') !== pointElRoot.data('nestable-id');
+
             /**
              * move vertical
              */
-            } else {
-                this.pointEl = $(document.elementFromPoint(e.pageX - this.w.scrollLeft(), e.pageY - this.w.scrollTop()));
-                if (this.pointEl.hasClass(this.options.handleClass)) {
-                    this.pointEl = $(this.pointEl[0].parentNode);
+            if (!mouse.dirAx || isNewRoot || isEmpty) {
+                // check if groups match if dragging over new root
+                if (isNewRoot && opt.group !== pointElRoot.data('nestable-group')) {
+                    return;
                 }
-                if (this.pointEl[0].nodeName.toLowerCase() !== this.options.itemNodeName || this.pointEl.hasClass(this.options.dragClass)) {
+                // check depth limit
+                depth = this.dragDepth - 1 + this.pointEl.parents(opt.listNodeName).length;
+                if (depth > opt.maxDepth) {
                     return;
                 }
                 var before = e.pageY < (this.pointEl.offset().top + this.pointEl.height() / 2);
                     parent = this.placeEl.parent();
-                    this.placeEl.parents(this.options.itemNodeName + ':first').after(this.placeEl);
-                    if (!parent.children().length) {
-                        this.unsetParent(parent.parent());
-                    }
-                if (before) {
+                // if empty create new list to replace empty placeholder
+                if (isEmpty) {
+                    list = $(document.createElement(this.options.listNodeName)).addClass(this.options.listClass);
+                    list.append(this.placeEl);
+                    this.pointEl.replaceWith(list);
+                }
+                else if (before) {
                     this.pointEl.before(this.placeEl);
-                } else {
+                }
+                else {
                     this.pointEl.after(this.placeEl);
+                }
+                if (!parent.children().length) {
+                    this.unsetParent(parent.parent());
+                }
+                if (!this.dragRootEl.find(opt.itemNodeName).length) {
+                    this.dragRootEl.append('<div class="' + opt.emptyClass + '"/>');
+                }
+                // parent root list has changed
+                if (isNewRoot) {
+                    this.hasNewRoot = true;
+                    this.dragRootEl = pointElRoot;
                 }
             }
         }
@@ -374,9 +454,10 @@
 
         lists.each(function()
         {
-            var plugin = $.data(this, 'plugin_nestable');
+            var plugin = $.data(this, 'nestable');
             if (!plugin) {
-                $.data(this, 'plugin_nestable', new Plugin(this, params));
+                $.data(this, 'nestable', new Plugin(this, params));
+                $.data(this, 'nestable-id', new Date().getTime());
             } else {
                 if (typeof params === 'string' && typeof plugin[params] === 'function') {
                     retval = plugin[params]();
