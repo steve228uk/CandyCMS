f5b07b7850184d3b1557dce05d4027b54895330e
Stephen Radford <steve228uk@gmail.com>
2012-08-26 15:21:56 +0100

    well that broke

--- plugins/Blog/Blog.php
+++ plugins/Blog/Blog.php
@@ -475,7 +475,7 @@
 	 	);
  	}
 
- 	return stripslashes($results);
+ 	return $results;
 
  }
  
@@ -498,7 +498,7 @@
   	
   	$ids = join(',', $return);
 
-  	return stripslashes(CandyDB::results("SELECT * FROM ". DB_PREFIX ."posts WHERE `post_id` IN (:ids) ORDER BY post_title ASC", compact('ids')));
+  	return CandyDB::results("SELECT * FROM ". DB_PREFIX ."posts WHERE `post_id` IN (:ids) ORDER BY post_title ASC", compact('ids'));
  
   }
   
